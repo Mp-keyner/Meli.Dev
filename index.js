591392d9050d69dba4c264df12e0a59e395a355b
Mp-keyner <keyneroswaldo0@gmail.com>
2023-06-10 20:53:24 -0500

    10 de junio 8:53

---
+++ index.js
@@ -0,0 +1,270 @@
+const openModal = document.getElementById('Map')
+const modal = document.querySelector('.modalMap')
+const closeModal = document.getElementById('MapClose')
+openModal.addEventListener('click', (e) => {
+    modal.classList.add('modal_open')
+    console.log('Accionaste el Map')
+})
+closeModal.addEventListener('click', (e) => {
+    modal.classList.remove('modal_open')
+})
+
+//2
+
+const openModal2 = document.getElementById('Filter')
+const modal2 = document.querySelector('.modalFilter')
+const closeModal2 = document.getElementById('FilterClose')
+openModal2.addEventListener('click', (e) => {
+    modal2.classList.add('modal_open')
+})
+closeModal2.addEventListener('click', (e) => {
+    modal2.classList.remove('modal_open')
+    console.log('first filter')
+})
+
+// 3
+
+const openModal3 = document.getElementById('ForEach')
+const modal3 = document.querySelector('.modalForEach')
+const closeModal3 = document.getElementById('ForEachClose')
+openModal3.addEventListener('click', (e) => {
+    modal3.classList.add('modal_open')
+})
+closeModal3.addEventListener('click', (e) => {
+    modal3.classList.remove('modal_open')
+    console.log('first ForEach')
+})
+
+// 4
+
+const openModal4 = document.getElementById('Find')
+const modal4 = document.querySelector('.modalFind')
+const closeModal4 = document.getElementById('FindClose')
+openModal4.addEventListener('click', (e) => {
+    modal4.classList.add('modal_open')
+})
+closeModal4.addEventListener('click', (e) => {
+    modal4.classList.remove('modal_open')
+    console.log('first Find')
+})
+
+// 5
+
+const openModal5 = document.getElementById('Sort')
+const modal5 = document.querySelector('.modalSort')
+const closeModal5 = document.getElementById('SortClose')
+openModal5.addEventListener('click', (e) => {
+    modal5.classList.add('modal_open')
+})
+closeModal5.addEventListener('click', (e) => {
+    modal5.classList.remove('modal_open')
+    console.log('first Sort')
+})
+
+// 6
+
+const openModal6 = document.getElementById('Some')
+const modal6 = document.querySelector('.modalSome')
+const closeModal6 = document.getElementById('SomeClose')
+openModal6.addEventListener('click', (e) => {
+    modal6.classList.add('modal_open')
+})
+closeModal6.addEventListener('click', (e) => {
+    modal6.classList.remove('modal_open')
+    console.log('first Some')
+})
+
+// 7
+
+const openModal7 = document.getElementById('Every')
+const modal7 = document.querySelector('.modalEvery')
+const closeModal7 = document.getElementById('EveryClose')
+openModal7.addEventListener('click', (e) => {
+    modal7.classList.add('modal_open')
+})
+closeModal7.addEventListener('click', (e) => {
+    modal7.classList.remove('modal_open')
+    console.log('first Every')
+})
+
+// 8
+
+const openModal8 = document.getElementById('Concat')
+const modal8 = document.querySelector('.modalConcat')
+const closeModal8 = document.getElementById('ConcatClose')
+openModal8.addEventListener('click', (e) => {
+    modal8.classList.add('modal_open')
+})
+closeModal8.addEventListener('click', (e) => {
+    modal8.classList.remove('modal_open')
+    console.log('first Concat')
+})
+
+// 9
+
+const openModal9 = document.getElementById('includes')
+const modal9 = document.querySelector('.modalincludes')
+const closeModal9 = document.getElementById('includesClose')
+openModal9.addEventListener('click', (e) => {
+    modal9.classList.add('modal_open')
+})
+closeModal9.addEventListener('click', (e) => {
+    modal9.classList.remove('modal_open')
+    console.log('first includes')
+})
+
+// 10
+
+const openModal10 = document.getElementById('Join')
+const modal10 = document.querySelector('.modalJoin')
+const closeModal10 = document.getElementById('JoinClose')
+openModal10.addEventListener('click', (e) => {
+    modal10.classList.add('modal_open')
+})
+closeModal10.addEventListener('click', (e) => {
+    modal10.classList.remove('modal_open')
+    console.log('first Join')
+})
+
+// 11
+
+const openModal11 = document.getElementById('Reduce')
+const modal11 = document.querySelector('.modalReduce')
+const closeModal11 = document.getElementById('ReduceClose')
+openModal11.addEventListener('click', (e) => {
+    modal11.classList.add('modal_open')
+})
+closeModal11.addEventListener('click', (e) => {
+    modal11.classList.remove('modal_open')
+    console.log('first Reduce')
+})
+
+// 12
+
+const openModal12 = document.getElementById('indexOf')
+const modal12 = document.querySelector('.modalindexOf')
+const closeModal12 = document.getElementById('indexOfClose')
+openModal12.addEventListener('click', (e) => {
+    modal12.classList.add('modal_open')
+})
+closeModal12.addEventListener('click', (e) => {
+    modal12.classList.remove('modal_open')
+    console.log('first indexOf')
+})
+
+// 13
+
+const openModal13 = document.getElementById('Push')
+const modal13 = document.querySelector('.modalPush')
+const closeModal13 = document.getElementById('PushClose')
+openModal13.addEventListener('click', (e) => {
+    modal13.classList.add('modal_open')
+})
+closeModal13.addEventListener('click', (e) => {
+    modal13.classList.remove('modal_open')
+    console.log('first Push')
+})
+
+// 14
+
+const openModal14 = document.getElementById('Pop')
+const modal14 = document.querySelector('.modalPop')
+const closeModal14 = document.getElementById('PopClose')
+openModal14.addEventListener('click', (e) => {
+    modal14.classList.add('modal_open')
+})
+closeModal14.addEventListener('click', (e) => {
+    modal14.classList.remove('modal_open')
+    console.log('first Pop')
+})
+
+// 15
+
+const openModal15 = document.getElementById('Shift')
+const modal15 = document.querySelector('.modalShift')
+const closeModal15 = document.getElementById('ShiftClose')
+openModal15.addEventListener('click', (e) => {
+    modal15.classList.add('modal_open')
+})
+closeModal15.addEventListener('click', (e) => {
+    modal15.classList.remove('modal_open')
+    console.log('first Shift')
+})
+
+// 16
+
+const openModal16 = document.getElementById('Unshift')
+const modal16 = document.querySelector('.modalUnshift')
+const closeModal16 = document.getElementById('UnshiftClose')
+openModal16.addEventListener('click', (e) => {
+    modal16.classList.add('modal_open')
+})
+closeModal16.addEventListener('click', (e) => {
+    modal16.classList.remove('modal_open')
+    console.log('first Unshift')
+})
+
+// 17
+
+const openModal17 = document.getElementById('Slice')
+const modal17 = document.querySelector('.modalSlice')
+const closeModal17 = document.getElementById('SliceClose')
+openModal17.addEventListener('click', (e) => {
+    modal17.classList.add('modal_open')
+})
+closeModal17.addEventListener('click', (e) => {
+    modal17.classList.remove('modal_open')
+    console.log('first Slice')
+})
+
+// 18
+
+const openModal18 = document.getElementById('Reverse')
+const modal18 = document.querySelector('.modalReverse')
+const closeModal18 = document.getElementById('ReverseClose')
+openModal18.addEventListener('click', (e) => {
+    modal18.classList.add('modal_open')
+})
+closeModal18.addEventListener('click', (e) => {
+    modal18.classList.remove('modal_open')
+    console.log('first Reverse')
+})
+
+// 19
+
+const openModal19 = document.getElementById('Splice')
+const modal19 = document.querySelector('.modalSplice')
+const closeModal19 = document.getElementById('SpliceClose')
+openModal19.addEventListener('click', (e) => {
+    modal19.classList.add('modal_open')
+})
+closeModal19.addEventListener('click', (e) => {
+    modal19.classList.remove('modal_open')
+    console.log('first Splice')
+})
+
+// 20
+
+const openModal20 = document.getElementById('lastindexOf')
+const modal20 = document.querySelector('.modallastindexOf')
+const closeModal20 = document.getElementById('lastindexOfClose')
+openModal20.addEventListener('click', (e) => {
+    modal20.classList.add('modal_open')
+})
+closeModal20.addEventListener('click', (e) => {
+    modal20.classList.remove('modal_open')
+    console.log('first lastindexOf')
+})
+
+// 21
+
+const openModal21 = document.getElementById('From')
+const modal21 = document.querySelector('.modalFrom')
+const closeModal21 = document.getElementById('FromClose')
+openModal21.addEventListener('click', (e) => {
+    modal21.classList.add('modal_open')
+})
+closeModal21.addEventListener('click', (e) => {
+    modal21.classList.remove('modal_open')
+    console.log('first From')
+})
